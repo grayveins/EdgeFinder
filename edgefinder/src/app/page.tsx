3c52933f6b0df778df28444413a8af78a4984777
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2025-01-25 03:28:22 -0500

    Add a couple more files and navbar

--- edgefinder/src/app/page.tsx
+++ edgefinder/src/app/page.tsx
@@ -1,15 +1,21 @@
+import { Navbar } from "./components/Navbar/Navbar";
+import React from "react";
+import './homepage.css'
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-gray-800">
-      <div className="flex flex-col items-center justify-center text-white">
-        <h1 className="text-7xl font-title">EdgeFinder</h1>
-        <h2 className="font-title p-4">Your ultimate sports betting assistant.</h2>
-        <div className="clickable">
-          <h2 className="text-2xl font-title">Enter.</h2>
+    <main>
+      <Navbar />
+
+      <div className="h-screen flex flex-col items-center justify-center bg-gray-800">
+        <div className="flex flex-col items-center justify-center text-white">
+          <h1 className="text-7xl font-title">EdgeFinder</h1>
+          <h2 className="font-title p-4">Your ultimate sports betting assistant.</h2>
+          <div className="clickable">
+            <h2 className="text-2xl font-title">Enter.</h2>
+          </div>
         </div>
-        
       </div>
-    </div>
+    </main>
   );
 }
