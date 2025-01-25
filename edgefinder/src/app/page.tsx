157b04d508847cb7794c168502f1b23c790b80d4
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2025-01-25 03:02:24 -0500

    Make changes to home page

--- edgefinder/src/app/page.tsx
+++ edgefinder/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="h-screen flex flex-col items-center justify-center bg-gray-800">
       <div className="flex flex-col items-center justify-center text-white">
         <h1 className="text-7xl font-title">EdgeFinder</h1>
-        <h2 className="font-title p-4">Your betting assistant.</h2>
+        <h2 className="font-title p-4">Your ultimate sports betting assistant.</h2>
         <div className="clickable">
           <h2 className="text-2xl font-title">Enter.</h2>
         </div>
