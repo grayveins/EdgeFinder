57d2fd588a638c65fb2fcc4efbfc1e77759cb033
Avstn1 <97896017+Avstn1@users.noreply.github.com>
2025-01-25 02:57:42 -0500

    Add main page

---
+++ edgefinder/src/app/main/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+export default function Main() {
+    return (
+      <div className="h-screen w-full justify-center flex bg-gray-800">
+        <div className="flex h-20 text-white">
+          <h1 className="text-5xl font-title">EdgeFinder</h1>
+        </div>
+      </div>
+    );
+  }
